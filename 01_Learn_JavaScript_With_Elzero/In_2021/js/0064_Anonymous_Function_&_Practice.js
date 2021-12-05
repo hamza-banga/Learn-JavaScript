db900774ee13871f1a8297b2a9ed05594ce254cd
حمزه بانقا <81154528+hamzoooz@users.noreply.github.com>
2021-12-05 06:29:46 -0800

    Update 0064_Anonymous_Function_&_Practice.js

--- 01_Learn_JavaScript_With_Elzero/In_2021/js/0064_Anonymous_Function_&_Practice.js
+++ 01_Learn_JavaScript_With_Elzero/In_2021/js/0064_Anonymous_Function_&_Practice.js
@@ -1,5 +1,4 @@
 /*
-
 Anonymous Function 
 Calling Named Function VS Anonymous Function 
 Argument To Other Function 
@@ -7,28 +6,30 @@ Task Without Name
 SetTimeout
 */
 
+// ====================================================
 // console.log( calculator( 10, 20 ) ); => Get Error Becuse Andefined Variable 
-
 let calculator = function ( num1, num2 ) {
     return num1 + num2 ;
 }
-console.log( calculator( 10, 20 ) ); // => 30
-// console.log(typeof calculator); // => Function 
 
+console.log( calculator( 10, 20 ) ); // => 30
+
+// ====================================================
 document.getElementById("show").onclick = function () {
     console.log( 10 + 20 ) ; // => 30
 }
+// ====================================================
+// setTimeFunction
+setTimeout( function () {
+    console.log("Good After 2 Seconds ")
+} ,2000 ); // Dleay 2 Seconds
+
+// ====================================================
+// Test To Get Input From User And Do Action
 
 let numone = document.getElementById("one").value
 let numtow = document.getElementById("tow").value;
-console.log(typeof numone);
 
 document.getElementById("calc").onclick = function () {
     console.log( numone + numtow ); // => num + num
 }
-
-
-setTimeout( function () {
-    console.log("Good After 2 Seconds ")
-} ,2000 )
-
