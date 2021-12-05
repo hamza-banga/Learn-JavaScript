af87bc8cbec5c0b77cc30756993f0adff70d2721
hamzoooz <hamzoooz.2020@gmail.com>
2021-12-05 08:39:26 -0800

    Try To Make Advanced

--- 01_Learn_JavaScript_With_Elzero/In_2021/js/0064_Anonymous_Function_&_Practice.js
+++ 01_Learn_JavaScript_With_Elzero/In_2021/js/0064_Anonymous_Function_&_Practice.js
@@ -5,55 +5,53 @@ Argument To Other Function
 Task Without Name 
 SetTimeout
 Try To Make Advanced 
- 
+BY @hamzoooz
 */
 
 // ====================================================
 // console.log( calculator( 10, 20 ) ); => Get Error Becuse Andefined Variable 
+
+//  Thiss Anonymous Function Without Name 
 let calculator = function ( num1, num2 ) {
     return num1 + num2 ;
 }
-
 console.log( calculator( 10, 20 ) ); // => 30
 
 // ====================================================
+// This Alsoe Anonymous Function Can Use Direct
 document.getElementById("show").onclick = function () {
     console.log( 10 + 20 ) ; // => 30
 }
 // ====================================================
-// setTimeFunction
+// setTimeFunction With Anonymous Function 
 setTimeout( function () {
     console.log("Good After 2 Seconds ")
 } ,2000 ); // Dleay 2 Seconds
 
 // ====================================================
+// Other Try To Make Advanced With Anonymous Function
+// ====================================================
 // Test To Get Input From User And Do Action
-
 let numone = document.getElementById("one").value
 let numtow = document.getElementById("tow").value;
-
 document.getElementById("calc").onclick = function () {
     console.log( numone + numtow ); // => num + num
 }
 
 // ====================================================
-// Can We Call Direct
-
+// We Can Call Direct
 function sayHello() {
-    console.log(` Hello Osama `)
+    console.log(` Hello Osama `) // -> Hello Osama 
 }
-
 document.getElementById("sayHello").onclick = sayHello() ;
 
 // ====================================================
 // Test To Get Input From User And Do Action
-
-let input = document.getElementById("input").value
-
-// let numtow = document.getElementById("tow").value;
+// Not Worh ( @hamzoooz  )
+let input = document.getElementById("input").value;
 document.getElementById("sayHello").onclick = function () {
     console.log( ` Hello ${input}` )
-};
+}
 
 document.getElementById("button").onclick = function () {
     console.log( input ); // => input
