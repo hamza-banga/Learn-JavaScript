5082a0c083c1126e31bb2d232807e8c7fc650816
حمزه بانقا <81154528+hamzoooz@users.noreply.github.com>
2021-12-06 01:00:49 -0800

    Arrow Function

--- 01_Learn_JavaScript_With_Elzero/In_2021/js/0066_Arrow_Function_Syntax.js
+++ 01_Learn_JavaScript_With_Elzero/In_2021/js/0066_Arrow_Function_Syntax.js
@@ -4,5 +4,25 @@
  *  Arrow Function 
  *  Regular VS Arrow [ Pram + No Pram ]
  *  Multiple Line 
- *  @hamzoooz
+ * 
+ * @hamzoooz
+ * 
  */
+
+
+
+// 
+// let print = function () {
+//     return 10;
+// };
+
+/**
+ * Arrow Function Can't Accept Pram
+ * When We Have One Statment We Can Put In One Line 
+ * We Can Remove Function And Return And Pracicts
+ */
+
+// When We Have One Statment We Can Put In One Line  
+let print = () =>  10 ;
+
+
