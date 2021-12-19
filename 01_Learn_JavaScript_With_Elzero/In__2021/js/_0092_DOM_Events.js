a42b547fb2178164d77c7af428c2b75d5520835b
hamzoooz <hamzoooz.2020@gmail.com>
2021-12-19 07:46:11 +0200

    DOM  [ Events ]

---
+++ 01_Learn_JavaScript_With_Elzero/In__2021/js/_0092_DOM_Events.js
@@ -0,0 +1,18 @@
+/**
+ * DOM [ Events ]
+ *  Use Events On HTML
+ *  Use Events On JS
+ *      onclick
+ *      oncontexmenu
+ *      onmouseenter
+ *      onmouseleave
+ * 
+ *      onload
+ *      onscroll
+ *      onresize
+ * 
+ *      onfocus
+ *      onblur
+ *      onsubmit
+ */
+
