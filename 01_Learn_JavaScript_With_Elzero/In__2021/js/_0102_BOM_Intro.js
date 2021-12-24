cdc08794c02f7d3c77e2be97e2de77125306647a
hamzoooz <hamzoooz.2020@gmail.com>
2021-12-24 20:03:22 +0200

    BOM

---
+++ 01_Learn_JavaScript_With_Elzero/In__2021/js/_0102_BOM_Intro.js
@@ -0,0 +1,25 @@
+/**
+ * BOM [ Browser Object Model ]
+ * -- Introduction 
+ * ---- Window Object Is The Browser Window 
+ * ---- Window Content The Document Object 
+ * ---- All Global Variables And Object and function are member of window object 
+ * ---- 
+ * -- What Can we do with window object ?
+ * ---- open window 
+ * ---- close window 
+ * ---- move window 
+ * ---- resize window 
+ * ---- print window 
+ * ---- run Code after preiod of time once or more 
+ * ---- fully cotrol the URL 
+ * ---- Save Data inside Browser To Use Later
+ */
+
+console.log("Good"); // -> Good
+window.console.log("Good"); // -> Good 
+
+window.document.title = "BOM Intro From JS File"
+
+
+
