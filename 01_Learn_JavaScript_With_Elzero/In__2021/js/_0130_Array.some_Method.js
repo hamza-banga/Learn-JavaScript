3cd9fc5494703ff42e01cd1efdcf0ed26d15435e
hamzoooz <hamzoooz.2020@gmail.com>
2021-12-26 17:32:31 +0200

    Arrey.every_Method

--- 01_Learn_JavaScript_With_Elzero/In__2021/js/_0130_Array.some_Method.js
+++ 01_Learn_JavaScript_With_Elzero/In__2021/js/_0130_Array.some_Method.js
@@ -12,7 +12,7 @@
  * -- Check if Number In Range 
  */
 
-let nums = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+let nums = [ 1, 2, 3, 4, 5, 6, 7, 8, 9,10  ]
 
 let check = nums.some(function(e){
    console.log(e)
@@ -23,10 +23,36 @@ let check = nums.some(function(e){
 
 let checkArow = nums.some( (e) => console.log(e > 5) );
 
-let print = nums.some(function(e){
-    if(e > 5){
-        console.log(` ${e} Grat Than 5 `);
-    }else{
-        console.log(` ${e} Less Than 5 `);
-    }
-})
+
+function checkValues(arr,val){
+    return arr.some(function(e){
+        return e === val;
+    });
+}
+console.log(checkValues( nums, 20 )); // -> false
+console.log(checkValues( nums, 5  )); // -> true
+
+
+let range = {
+    min:10,
+    max:20
+};
+let checkRange = nums.some( function(e){
+    // console.log(this.min);
+    // console.log(this.max);
+    return e >= this.min && e <= this.max;
+},range)
+
+console.log(checkRange); // -> true
+
+
+
+
+
+// let print = nums.some(function(e){
+//     if(e > 5){
+//         console.log(` ${e} Grat Than 5 `);
+//     }else{
+//         console.log(` ${e} Less Than 5 `);
+//     }
+// })
